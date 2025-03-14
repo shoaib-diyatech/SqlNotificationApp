33e7f60e0d27b8c60e62d6a3e812fbc8f85dec97
Muhammad Shoaib Khalid <shoaib_khalid@diyatech.org.pk>
2025-03-14 09:49:01 +0500

    Initial commit

---
+++ SqlNotificationApp/DatabaseService.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+public class DatabaseService
+{
+    private readonly string _connectionString;
+
+    public DatabaseService(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DefaultConnection");
+    }
+
+    public void Connect()
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Connected to database!");
+        }
+    }
+}
